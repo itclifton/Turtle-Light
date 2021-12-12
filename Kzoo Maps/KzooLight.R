66e1cdd0bba5f5c3e996eb3e5d7f5eeed6ee882c
itclifton <ian.t.clifton@gmail.com>
2021-12-12 16:12:51 -0500

    Includes time spent dry

--- Kzoo Maps/KzooLight.R
+++ Kzoo Maps/KzooLight.R
@@ -1,10 +1,13 @@
-setwd("~/Desktop/Research/Side Projects/Turtle Light 2021/Kzoo Maps")
+setwd("~/Desktop/Research/Side Projects/Turtle Light 2021/Turtle-Light/Kzoo Maps")
 ## Packages ----
 library(tidyr)
 library(tidyverse)
 library(ggplot2)
 library(lme4)
+library(lmerTest)
+library(emmeans)
 st.err = function(x) {sd(x)/sqrt(length(x))}
+path<-"/Users/ianclifton/Desktop/Research/Side Projects/Turtle Light 2021/Figures"
 
 ## Data Management ----
 Alvin<-read.csv("Alvin.csv")
@@ -70,14 +73,14 @@ Turtles<-Turtles[,c(2,5,7,13,14)]
 Turtles$datetime<-as.POSIXct(strptime(Turtles$Adjusted.Local.Time, format("%m/%d/%y %H:%M")))
 Turtles$WetDry<-fct_recode(Turtles$Wet.Dry, "0"="wet", "1"="dry")
 Turtles<-Turtles[,c(4,5,6,7,2)]
-Turtles$WetDry<-as.numeric(as.character(Turtles$WetDry))
+# Turtles$WetDry<-as.numeric(as.character(Turtles$WetDry))
 Turtles$Week<-as.numeric(strftime(Turtles$datetime,format="%W"))
 
 
 ## Preliminary Light Analysis ----
 # Light Only - I'm not sure what duration means.
 Turtles.lux<-Turtles[,c(1,2,3,5,6)]
-Turtles.lux<-na.omit(Turtles)
+Turtles.lux<-na.omit(Turtles.lux)
 # Remove nighttime based on mean date sunrise (6:06) and sunset (21:22)
 Turtles.Day<-subset(Turtles.lux, format(datetime, "%H:%M:%S")<"21:22:00" & format(datetime, "%H:%M:%S")>"06:06:00")
 
@@ -108,14 +111,36 @@ ggplot(data=Monthly.Sex, aes(x=Month, y=Mean, group=sex))+
 Weekly.Sex.Mean<-aggregate(Mean~sex+Week, mean, data=Ind.Summary)
 Weekly.Sex.se<-aggregate(Mean~sex+Week, st.err, data=Ind.Summary)
 Weekly.Sex<-cbind(Weekly.Sex.Mean, Weekly.Sex.se$Mean)
-colnames(Weekly.Sex)<-c("sex","Week","Mean","SE")
-Weekly.Sex<-subset(Weekly.Sex, Week!="17")
+colnames(Weekly.Sex)<-c("Sex","Week","Mean","SE")
+Weekly.Sex<-subset(Weekly.Sex, Week!="17" & Week!="34")
 #Day.Sex.Mean$Month<-as.Date(Day.Sex.Mean$Month, format="%m")
-ggplot(data=Weekly.Sex, aes(x=Week, y=Mean, group=sex))+
-  geom_point(aes(colour=sex), size=3, position=position_dodge(width=0.5))+
-  geom_errorbar(aes(ymin=Mean-SE, ymax=Mean+SE), width=.5, position=position_dodge(width=0.5))+
-  ylab("Mean Daily Light")
-summary(lmer(Mean~sex+Week+(1|ID), data=Ind.Summary))
+p1<-ggplot(data=Weekly.Sex, aes(x=Week, y=Mean, group=Sex))+
+  annotate("rect", xmin=22.6, xmax=24.5, ymin=-Inf, ymax=Inf, alpha=0.2, fill="blue")+
+  annotate("rect", xmin=27.3, xmax=29.7, ymin=-Inf, ymax=Inf, alpha=0.2, fill="red")+
+  geom_errorbar(aes(ymin=Mean-SE, ymax=Mean+SE), width=.5, position=position_dodge(width=0.2))+
+  geom_point(aes(colour=Sex), size=7, position=position_dodge(width=0.2))+
+  theme_bw()+
+  theme(panel.grid.major=element_line(colour="#FFFFFF"),panel.grid.minor=element_line(colour="#FFFFFF"))+
+  theme(axis.text=element_text(size=20,face="bold"), axis.title=element_text(size=30,face="bold"),
+        legend.text=element_text(size=20, face="bold"), legend.title=element_text(size=26, face="bold"))+
+  geom_line(aes(colour=Sex), position=position_dodge(w=0.2), size=2)+
+  theme(legend.position="bottom")+
+  ylab("Mean Daily Light (lx)")
+#ggsave(path=path, filename="WeeklyLight.png", height=14, width=20, plot=p1)
+# Data analysis
+Ind.Summary1<-subset(Ind.Summary, Week!="17" & Week!="34")
+Ind.Summary1$Week<-as.factor(Ind.Summary1$Week)
+lm1<-lmer(Mean~Week+(1|sex)+(1|ID), data=Ind.Summary1)
+summary(lm1)
+em.m1<-emmeans(lm1, c("Week"))
+summary(em.m1)
+contrast(em.m1, 'tukey')
+anova(lm1, type=3)
+# End of season divergence
+Ind.Summary2<-subset(Ind.Summary, Week>27 & Week<33)
+lm2<-lmer((Mean/1000)~sex*Week+(1|sex:ID), data=Ind.Summary2)
+summary(lm2)
+
 
 Daily.Sex.Mean<-aggregate(Mean~sex+format(Day, "%m-%d"), mean, data=Ind.Summary)
 Daily.Sex.se<-aggregate(Mean~sex+format(Day, "%m-%d"), st.err, data=Ind.Summary)
@@ -141,8 +166,72 @@ ggplot(x=Day, y=Light, group=sex, data=Day.Sex.median)+
   geom_line(aes(x=Day, y=Light, color=sex))+
   ylab("Median Daily Light")
 
-
 ## Preliminary Wet/Dry Analysis ----
 
 
 
+
+Turtles.dur<-rbind(Alvin, April, Betty, Bugs,
+                   Carmen, Chip, Dale, Dot,
+                   Ernie, Felix, Fiona,
+                   Granny, Grouch, Hubert, Marlo,
+                   Satan, Ziggy)
+Turtles.dur$ID<-as.factor(Turtles.dur$ID)
+Turtles.dur$datetime<-as.POSIXct(strptime(Turtles.dur$Adjusted.Local.Time, format("%m/%d/%y %H:%M")))
+Turtles.dur<-subset(Turtles.dur, Light!="NA")
+
+Turtles.dur.Daily<-aggregate(Light~Wet.Dry+sex+ID+format(datetime, "%m-%d"), length, data=Turtles.dur)
+Turtles.Wet<-subset(Turtles.dur.Daily, Wet.Dry=="wet")
+Turtles.Dry<-subset(Turtles.dur.Daily, Wet.Dry=="dry")
+Turtles.Dry$Prop<-Turtles.Dry$Light/288
+Turtles.Dry$datetime<-as.Date(Turtles.Dry$`format(datetime, "%m-%d")`, format("%m-%d"))
+Turtles.Dry$Week<-as.numeric(strftime(Turtles.Dry$datetime,format="%W"))
+
+Prop.Sex.Mean<-aggregate(Prop~sex+Week, mean, data=Turtles.Dry)
+Prop.Sex.se<-aggregate(Prop~sex+Week, st.err, data=Turtles.Dry)
+Prop.Sex<-cbind(Prop.Sex.Mean, Prop.Sex.se$Prop)
+colnames(Prop.Sex)<-c("Sex","Week","Mean","SE")
+Prop.Sex<-subset(Prop.Sex, Week!="17" & Week !="34")
+
+ggplot(data=Prop.Sex, aes(x=Week, y=Mean, group=Sex))+
+  annotate("rect", xmin=22.6, xmax=24.5, ymin=-Inf, ymax=Inf, alpha=0.2, fill="blue")+
+  annotate("rect", xmin=27.3, xmax=29.7, ymin=-Inf, ymax=Inf, alpha=0.2, fill="red")+
+  geom_errorbar(aes(ymin=Mean-SE, ymax=Mean+SE), width=.5, position=position_dodge(width=0.2))+
+  geom_point(aes(colour=Sex), size=7, position=position_dodge(width=0.2))+
+  theme_bw()+
+  theme(panel.grid.major=element_line(colour="#FFFFFF"),panel.grid.minor=element_line(colour="#FFFFFF"))+
+  theme(axis.text=element_text(size=16,face="bold"), axis.title=element_text(size=22,face="bold"),
+        legend.text=element_text(size=16, face="bold"), legend.title=element_text(size=16, face="bold"))+
+  geom_line(aes(colour=Sex), position=position_dodge(w=0.2), size=2)+
+  theme(legend.position="bottom")+
+  ylab("Mean Daily Proportion Dry")
+
+# Daytime only
+Turtles.Dur.Daytime<-subset(Turtles.dur, format(datetime, "%H:%M:%S")<"21:22:00" & format(datetime, "%H:%M:%S")>"06:06:00")
+Turtles.dur.Daytime.Daily<-aggregate(Light~Wet.Dry+sex+ID+format(datetime, "%m-%d"), length, data=Turtles.Dur.Daytime)
+
+Turtles.Wet.Daytime<-subset(Turtles.dur.Daytime.Daily, Wet.Dry=="wet")
+Turtles.Dry.Daytime<-subset(Turtles.dur.Daytime.Daily, Wet.Dry=="dry")
+Turtles.Dry.Daytime$Prop<-Turtles.Dry.Daytime$Light/183
+Turtles.Dry.Daytime$datetime<-as.Date(Turtles.Dry.Daytime$`format(datetime, "%m-%d")`, format("%m-%d"))
+Turtles.Dry.Daytime$Week<-as.numeric(strftime(Turtles.Dry.Daytime$datetime,format="%W"))
+
+Prop.Sex.Mean.Day<-aggregate(Prop~sex+Week, mean, data=Turtles.Dry.Daytime)
+Prop.Sex.se.Day<-aggregate(Prop~sex+Week, st.err, data=Turtles.Dry.Daytime)
+Prop.Sex<-cbind(Prop.Sex.Mean.Day, Prop.Sex.se.Day$Prop)
+colnames(Prop.Sex)<-c("Sex","Week","Mean","SE")
+Prop.Sex<-subset(Prop.Sex, Week!="17" & Week!="34")
+
+p2<-ggplot(data=Prop.Sex, aes(x=Week, y=Mean, group=Sex))+
+  annotate("rect", xmin=22.6, xmax=24.5, ymin=-Inf, ymax=Inf, alpha=0.2, fill="blue")+
+  annotate("rect", xmin=27.3, xmax=29.7, ymin=-Inf, ymax=Inf, alpha=0.2, fill="red")+
+  geom_errorbar(aes(ymin=Mean-SE, ymax=Mean+SE), width=.5, position=position_dodge(width=0.2))+
+  geom_point(aes(colour=Sex), size=7, position=position_dodge(width=0.2))+
+  theme_bw()+
+  theme(panel.grid.major=element_line(colour="#FFFFFF"),panel.grid.minor=element_line(colour="#FFFFFF"))+
+  theme(axis.text=element_text(size=20,face="bold"), axis.title=element_text(size=30,face="bold"),
+        legend.text=element_text(size=20, face="bold"), legend.title=element_text(size=26, face="bold"))+
+  geom_line(aes(colour=Sex), position=position_dodge(w=0.2), size=2)+
+  theme(legend.position="bottom")+
+  ylab("Mean Proportion of Time Spent Dry")
+#ggsave(path=path, filename="WeeklyDry.png", height=14, width=20, plot=p2)
